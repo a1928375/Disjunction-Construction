d32dcce4dbe5a59e4b768cfcdd17aed04a511195
Arthur Shih <a1928375@hotmail.com>
2019-09-28 11:58:28 +0800

    Add files via upload

---
+++ Hyphenation.py
@@ -0,0 +1,11 @@
+import re
+
+regexp = r"[a-z]+-?[a-z]+"
+
+print (re.findall(regexp,"well-liked") == ["well-liked"])
+
+print (re.findall(regexp,"html") == ["html"])
+
+print (re.findall(regexp,"a-b-c") != ["a-b-c"])
+
+print (re.findall(regexp,"a--b") != ["a--b"])
