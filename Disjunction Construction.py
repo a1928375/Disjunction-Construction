6a4127c4a31a5bc4d70957196a5ee16f9dd10344
Arthur Shih <a1928375@hotmail.com>
2019-09-28 11:55:18 +0800

    Add files via upload

---
+++ Disjunction Construction.py
@@ -0,0 +1,16 @@
+import re
+
+regexp = r"[a][b]|[0-9]+"
+
+
+print (re.findall(regexp,"ab") == ["ab"])
+
+print (re.findall(regexp,"1") == ["1"])
+
+print (re.findall(regexp,"123") == ["123"])
+
+print (re.findall(regexp,"a") != ["a"])
+
+print (re.findall(regexp,"abc") != ["abc"])
+
+print (re.findall(regexp,"abc123") != ["abc123"])
